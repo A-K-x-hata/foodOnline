09b1a7106a07096e3848da9b22256a113cbb56f9
A-K-x-hata <akshatadileep@gmail.com>
2024-02-04 00:29:51 +0530

    vendor profile form and validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
 class UserForm(forms.ModelForm):
     password=forms.CharField(widget=forms.PasswordInput())
     confirm_password=forms.CharField(widget=forms.PasswordInput())
@@ -16,3 +16,19 @@ class UserForm(forms.ModelForm):
     
         if password != confirm_password:
                 raise forms.ValidationError("Passwords don't match!")
+        
+class UserProfileForm(forms.ModelForm):
+    address=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Start typing...','required':'required'}))
+    profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
+    cover_photo = forms.ImageField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
+   
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city',
+                  'pincode', 'latitude', 'longitude']
+        
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
